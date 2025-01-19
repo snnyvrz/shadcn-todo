86137c15ebfb91befa756148e398ebea7c2c6fb9
sinaniya <sinaniya@gmail.com>
2025-01-19 21:06:26 +0100

    created the add-todo-item form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+  todoItem: z.string().min(2).max(50),
+});
 
 function App() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      todoItem: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
     <div className="max-w-screen-lg mx-auto p-4">
       <NavigationMenu>
@@ -15,9 +40,24 @@ function App() {
           <NavigationMenuItem>Hello, Sina!</NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
-      <Label>Add Todo Item:</Label>
-      <Input />
-      <Button>Add</Button>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="todoItem"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Todo Item</FormLabel>
+                <FormControl>
+                  <Input placeholder="Todo" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
   );
 }

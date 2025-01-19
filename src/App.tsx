4a8d15ff1eae1daaaf5c5ff9aa8b5b8b35250919
sinaniya <sinaniya@gmail.com>
2025-01-19 14:40:41 +0100

    added label and button

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import {
   NavigationMenuItem,
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <div className="mx-auto p-4">
+    <div className="max-w-screen-lg mx-auto p-4">
       <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>Hello, Sina!</NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
+      <Label>Add Todo Item:</Label>
+      <Input />
+      <Button>Add</Button>
     </div>
   );
 }

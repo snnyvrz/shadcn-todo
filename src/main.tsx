62fb4fd9bc07c2b2ac16a1b2992a4ccc56a8c7fd
sinaniya <sinaniya@gmail.com>
2025-01-20 23:53:07 +0100

    added react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="signin" element={<div>Sign In</div>} />
+        <Route path="signup" element={<div>Sign Up</div>} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);

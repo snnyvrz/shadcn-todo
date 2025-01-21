ee3772657502b6fcc08bc20ea353bd413d5b2add
sinaniya <sinaniya@gmail.com>
2025-01-21 22:16:50 +0100

    added tanstack query for fetch & cache

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,12 +4,45 @@ import {
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 import { LogOut } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
 
 export const Navbar = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      try {
+        const response = await fetch("http://localhost:8000");
+        return await response.json();
+      } catch (error) {
+        console.error("Error fetching user data", error);
+      }
+    },
+  });
+
+  if (isPending) {
+    return (
+      <NavigationMenu className="bg-primary text-white p-4 mb-4 max-w-full justify-between rounded-b-lg">
+        <NavigationMenuList>
+          <NavigationMenuItem>Loading...</NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    );
+  }
+
+  if (error) {
+    return (
+      <NavigationMenu className="bg-primary text-white p-4 mb-4 max-w-full justify-between rounded-b-lg">
+        <NavigationMenuList>
+          <NavigationMenuItem>Error fetching user data</NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    );
+  }
+
   return (
     <NavigationMenu className="bg-primary text-white p-4 mb-4 max-w-full justify-between rounded-b-lg">
       <NavigationMenuList>
-        <NavigationMenuItem>Hello, Sina!</NavigationMenuItem>
+        <NavigationMenuItem>Hello, {data.Hello}!</NavigationMenuItem>
       </NavigationMenuList>
       <NavigationMenuList>
         <NavigationMenuItem>
--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,22 @@ import "./index.css";
 import App from "./App.tsx";
 import { Signin } from "@/components/Signin.tsx";
 import { Signup } from "@/components/Signup.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-        <Route path="signin" element={<Signin />} />
-        <Route path="signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<App />} />
+          <Route path="signin" element={<Signin />} />
+          <Route path="signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );

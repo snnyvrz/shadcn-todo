94ab26026f264d5b43587ba1b27a2306f667b646
sinaniya <sinaniya@gmail.com>
2025-01-26 02:34:23 +0100

    added some animation to alert component

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,50 +1,61 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useContext } from "react";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
 import { AlertCircle, CircleX } from "lucide-react";
 import { AlertContext } from "@/lib/AlertContext";
+import { cn } from "@/lib/utils";
+import * as motion from "motion/react-client";
+import { AnimatePresence } from "motion/react";
 
 export const AlertComponent = () => {
   const {
-    state: { show, type_, message },
+    state: { show, type_, message, progress },
     dispatch,
   } = useContext(AlertContext);
 
-  const [progress, setProgress] = useState(100);
-
   useEffect(() => {
     const interval = setInterval(() => {
-      setProgress((prev) => prev - 2);
+      dispatch({ type: "SET_PROGRESS", payload: progress - 2 });
     }, 100);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [progress, dispatch]);
 
   useEffect(() => {
     if (progress === 0) {
       dispatch({ type: "HIDE_ALERT" });
-      setProgress(100);
     }
   }, [progress, dispatch]);
 
   return (
-    <div className="fixed top-4 flex items-center justify-center w-screen z-50">
-      <Alert className="max-w-lg shadow-lg p-0" hidden={!show} variant={type_}>
-        <Button
-          className={`absolute top-0 right-0 hover:bg-${type_}`}
-          variant="ghost"
-          onClick={() => dispatch({ type: "HIDE_ALERT" })}
-        >
-          <CircleX className="w-4 h-4" />
-        </Button>
-        <AlertCircle className="h-4 w-4" />
-        <AlertTitle className="m-4">
-          {type_.charAt(0).toUpperCase() + type_.slice(1)}
-        </AlertTitle>
-        <AlertDescription className="m-4">{message}</AlertDescription>
-        <Progress value={progress} className="w-full" variant={type_} />
-      </Alert>
-    </div>
+    <AnimatePresence>
+      <motion.div
+        className="fixed top-4 flex items-center justify-center w-screen z-50"
+        initial={{ y: -100 }}
+        animate={{ y: show ? 0 : -200 }}
+        transition={{ duration: 0.3 }}
+        exit={{ y: -200 }}
+      >
+        <Alert className="max-w-lg shadow-lg p-0" variant={type_}>
+          <Button
+            className={cn(
+              type_ === "success" ? "hover:bg-success/10" : "hover:bg-error/10",
+              "absolute top-0 right-0"
+            )}
+            variant="ghost"
+            onClick={() => dispatch({ type: "HIDE_ALERT" })}
+          >
+            <CircleX className="w-4 h-4" />
+          </Button>
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle className="m-4">
+            {type_.charAt(0).toUpperCase() + type_.slice(1)}
+          </AlertTitle>
+          <AlertDescription className="m-4">{message}</AlertDescription>
+          <Progress value={progress} className="w-full" variant={type_} />
+        </Alert>
+      </motion.div>
+    </AnimatePresence>
   );
 };
--- src/lib/AlertContext.tsx
+++ src/lib/AlertContext.tsx
@@ -4,16 +4,19 @@ interface AlertState {
   show: boolean;
   type_: "success" | "error";
   message: string;
+  progress: number;
 }
 
 type AlertActions =
-  | { type: "SHOW_ALERT"; payload: Omit<AlertState, "show"> }
-  | { type: "HIDE_ALERT" };
+  | { type: "SHOW_ALERT"; payload: Omit<AlertState, "show" | "progress"> }
+  | { type: "HIDE_ALERT" }
+  | { type: "SET_PROGRESS"; payload: number };
 
 export const initialAlertState: AlertState = {
   show: false,
   type_: "success",
   message: "",
+  progress: 100,
 };
 
 export const AlertContext = createContext<{
@@ -31,9 +34,19 @@ export const alertReducer = (state: AlertState, action: AlertActions) => {
         show: true,
         type_: action.payload.type_,
         message: action.payload.message,
+        progress: 100,
       };
     case "HIDE_ALERT":
-      return initialAlertState;
+      return {
+        ...state,
+        show: false,
+        progress: 0,
+      };
+    case "SET_PROGRESS":
+      return {
+        ...state,
+        progress: action.payload,
+      };
     default:
       return state;
   }
